d392282d79177b1f7664295a4efd77c8997cfc03
kishsaver <kishsaverf62@gmail.com>
2025-02-10 19:30:11 +0900

    add:init.sql for mysqlカラム文字数

--- flask_app/models.py
+++ flask_app/models.py
@@ -10,7 +10,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     musics = db.relationship('Music', backref='user', lazy=True)
 
     # パスワードハッシュ系、ルーティングファイル側で実装も可
